e09006bf531808fbfc1d12ee25f40bb63237257f
Bruno Martins <obruno26@outlook.com>
2024-05-03 20:52:21 -0300

    [03-05-2024]Commit inicial, criando microserviço de envio de email em java

---
+++ src/main/java/com/email/repository/EmailRepository.java
@@ -0,0 +1,9 @@
+package com.email.repository;
+
+import com.email.model.EmailModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
+}
